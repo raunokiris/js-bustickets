d34b97501a75681c2749650a0709607f20bef679
Rauno Kiris <rauno.kiris@gmail.com>
2017-12-12 00:18:28 +0200

    0.0.5: Fix PINS code check failure bug (luxexpress.eu); automatically add delivery email address (luxexpress.eu)

--- js/content_lux.js
+++ js/content_lux.js
@@ -1,72 +1,128 @@
-document.querySelector("[data-bind$=RemovePassenger]").addEventListener('click', reloadButtons);
-document.querySelector("[data-bind$=AddPassenger]").addEventListener('click', reloadButtons);
+const currentPath = window.location.pathname;
 
-setTimeout(function(){
-	reloadButtons();
-}, 1000);
+if (currentPath.startsWith("/vali-reisijad/")) {
 
-function reloadButtons() {
-	setTimeout(function(){
-		let tables = document.getElementsByClassName("passenger-info");  // Get all passanger-info tables
-		for (let i = 0; i < tables.length; i++) {  // Loop trough all tables and add button.
-			removeOldButton(tables[i]);
-			
-			let button_id = `btn-loadData_${i}`;
-			addNewButton(tables[i], button_id);
-			linkButtonToLoadData(tables[i], button_id);
-		}
-	}, 1000);
-}
+    document.querySelector("[data-bind$=RemovePassenger]").addEventListener('click', reloadButtons);
+    document.querySelector("[data-bind$=AddPassenger]").addEventListener('click', reloadButtons);
 
-function removeOldButton(table) {
-	let loadDataDivs = table.getElementsByClassName('loadData');
-	if (loadDataDivs.length !== 0) {
-		loadDataDivs[0].parentNode.removeChild(loadDataDivs[0])
-	}
-}
+    setTimeout(function () {
+        reloadButtons();
+    }, 1000);
 
-function addNewButton(table, button_id) {
-	table.getElementsByClassName("contacting-options-fields")[0].innerHTML += `
+    function reloadButtons() {
+        setTimeout(function () {
+            let tables = document.getElementsByClassName("passenger-info");  // Get all passanger-info tables
+            for (let i = 0; i < tables.length; i++) {  // Loop trough all tables and add button.
+                removeOldButton(tables[i]);
+
+                let button_id = `btn-loadData_${i}`;
+                addNewButton(tables[i], button_id);
+                linkButtonToLoadData(tables[i], button_id);
+            }
+        }, 1000);
+    }
+
+    function removeOldButton(table) {
+        let loadDataDivs = table.getElementsByClassName('loadData');
+        if (loadDataDivs.length !== 0) {
+            loadDataDivs[0].parentNode.removeChild(loadDataDivs[0])
+        }
+    }
+
+    function addNewButton(table, button_id) {
+        table.getElementsByClassName("contacting-options-fields")[0].innerHTML += `
 		<div class="col-xs-12 pad-5-i text-right loadData">
 			<button class="btn pad-0-30 loadData" id="${button_id}" title="Lae andmed" type="button" style="margin-right: 10px">Lae andmed</button>
 		</div>
 		`;
+    }
+
+    function linkButtonToLoadData(table, button_id) {
+        let button_elem = document.getElementById(button_id);
+        button_elem.addEventListener('click', function () {
+            load_data(table)
+        }, false);
+    }
+
+    function load_data(table) {
+        chrome.storage.sync.get({
+            FirstName: '',
+            LastName: '',
+            Email: '',
+            PhonePrefix: '',
+            PhoneNumber: '',
+            DeliverEmailAddress: '',
+            BonusCardNumber: ''
+        }, function (data) {
+            insertUserData(table, data);
+            confirmPins(table);
+            saveTimestamp()
+        });
+    }
+
+    function insertUserData(table, user) {
+        try {
+            if (user.FirstName === '' && user.LastName === '') {
+                openOptionsPage()
+            } else {
+                changeElementValue(table.querySelector("[data-bind$=firstName]"), user.FirstName);
+                changeElementValue(table.querySelector("[data-bind$=lastName]"), user.LastName);
+                changeElementValue(table.querySelector("[data-bind$=BalticMilesNumber]"), user.BonusCardNumber);
+                changeElementValue(table.querySelector("[data-bind^=validationElement]"), user.PhonePrefix);
+                changeElementValue(table.querySelector("[data-bind$=phonePrefix]"), user.PhonePrefix);
+                changeElementValue(table.querySelector("[data-bind$=email]"), user.Email);
+                changeElementValue(table.querySelector("[data-bind$=phoneNumber]"), user.PhoneNumber);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    function confirmPins(table) {
+        table.querySelector("input.confirm_change").click();
+    }
+
+    function saveTimestamp() {
+        chrome.storage.sync.set({
+            LastLoadTimestamp: Math.floor(Date.now() / 1000)
+        });
+    }
+
+    function openOptionsPage() {
+        let optionsUrl = chrome.extension.getURL("options/options.html");
+        window.open(optionsUrl, "_blank", "width=340,height=340");
+    }
 }
 
-function linkButtonToLoadData(table, button_id) {
-	let button_elem = document.getElementById(button_id);
-	button_elem.addEventListener('click', function () {load_data(table)}, false);
+else if (currentPath.startsWith("/ostukorv/ulevaade-ja-maksa")) {
+    chrome.storage.sync.get({
+        LastLoadTimestamp: 0
+    }, function (data) {
+        let currentTimestamp = Math.floor(Date.now() / 1000);
+        if (currentTimestamp - data.LastLoadTimestamp <= 5 * 60) {
+            insertEmailAddress();
+        }
+
+    });
+
+    function insertEmailAddress() {
+        chrome.storage.sync.get({
+            FirstName: '',
+            LastName: '',
+            DeliverEmailAddress: ''
+        }, function (user) {
+            try {
+                if (user.FirstName !== '' && user.LastName !== '') {
+                    changeElementValue(document.getElementById("DeliverEmailAddress"), user.DeliverEmailAddress);
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        });
+    }
 }
 
-function load_data(table) {
-	chrome.storage.sync.get({
-		FirstName: '',
-		LastName: '',
-		Email: '',
-		PhonePrefix: '',
-		PhoneNumber: '',
-		DeliverEmailAddress: '',
-		BonusCardNumber: ''
-	}, function (user) {
-		try {
-			if (user.FirstName === '' && user.LastName === '') {
-            	openOptionsPage()
-        	} else {
-				table.querySelector("[data-bind$=firstName]").value = user.FirstName;
-				table.querySelector("[data-bind$=lastName]").value = user.LastName;
-				table.querySelector("[data-bind$=BalticMilesNumber]").value = user.BonusCardNumber;
-				table.querySelector("[data-bind^=validationElement]").value = user.PhonePrefix;
-				table.querySelector("[data-bind$=phonePrefix]").value = user.PhonePrefix;
-				table.querySelector("[data-bind$=email]").value = user.Email;
-				table.querySelector("[data-bind$=phoneNumber]").value = user.PhoneNumber
-			}
-		} catch (e) {
-			console.log(e);
-		}
-	});
-}
-
-function openOptionsPage() {
-    let optionsUrl = chrome.extension.getURL("options/options.html");
-    window.open(optionsUrl, "_blank", "width=340,height=340");
+function changeElementValue(el, new_value) {
+    el.value = new_value;
+    el.dispatchEvent(new KeyboardEvent('change', {'key': 'a'}));
 }
